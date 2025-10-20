70b6a678dd459125437f4dadc80e7ebb7b76b4ae
Chrinux-AI <christolabiyi35@gmail.com>
2025-10-20 11:01:06 +0100

    Updated

---
+++ Level2/Javascript/calc1/script.js
@@ -0,0 +1,19 @@
+// const myFriend = "Victory"
+// console.log(showMyFriend.innerHTML);
+
+// showMyFriend.innerHTML = myFriend
+
+// console.log(firstNumber.value);
+const multiply = () => {
+    console.log(firstNumber.value);
+    console.log(secondNumber.value);
+    const answer = firstNumber.value * secondNumber.value
+    console.log(answer);
+    showResult.innerHTML = answer
+}
+
+const div = () => {
+    console.log(firstNumber.value);
+    
+    
+}
---
+++ Level2/Javascript/correction/script.js
@@ -0,0 +1,26 @@
+const areaofACircle = () =>{
+    const pi = 22/7
+    const radius = Number(prompt('Enter the value of the radius'))
+    // console.log(radius);
+    const answer = (pi * (radius**2)).toFixed(2)
+    console.log(answer + 'cm²');
+    
+
+}
+
+const areaofATriangle = () =>{
+    alert("triangle")
+}
+
+const bmi = () =>{
+    alert("bmi")
+}
+
+const bestClubInTheWorld = "Chelsea"
+    console.log(bestClubInTheWorld);
+
+const testString = "78"
+    console.log(typeof(testString));
+
+    const convertedString = parseFloat()
+    console.log(bestClubInTheWorld);
